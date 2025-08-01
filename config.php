bf48a00b8189bce550e2b56cb49884f87e900986
R3ndly <akalinart020@gmail.com>
2025-08-01 22:04:58 +0800

    Файл кофигрукации для получение доступов

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'database' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => 'Q1qqqqqq',
+        'name' => 'inline',
+        'charset' => 'utf8mb4'
+    ],
+    'api' => [
+        'posts_url' => 'https://jsonplaceholder.typicode.com/posts',
+        'comments_url' => 'https://jsonplaceholder.typicode.com/comments'
+    ]
+];
+//Этот файл должен быть добавлен в .gitignore
