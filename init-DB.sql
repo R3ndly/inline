3fb8e0186652bfcb7a91cfd3dfdff2bdc2f1433e
R3ndly <akalinart020@gmail.com>
2025-08-01 18:17:50 +0800

    Файл для дампа в БД

--- init-DB.sql
+++ init-DB.sql
@@ -6,25 +6,13 @@ COLLATE utf8mb4_unicode_ci;
 -- Использование созданной БД
 USE inline;
 
---Создание таблицы users(можно не создавать, т.к. я не увидил в этом необходимости)
-CREATE TABLE IF NOT EXISTS users (
-	userId INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL,
-	password VARCHAR(255) NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (userId),
-	UNIQUE KEY (email)
-) ENGINE=InnoDB;
-
 --Создание таблицы articles
 CREATE TABLE IF NOT EXISTS articles (
 	id INT NOT NULL AUTO_INCREMENT,
 	userId INT NOT NULL,
 	title VARCHAR(255) NOT NULL,
 	body TEXT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (userId) REFERENCES users(userId)
+	PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
 --Создание таблицы comments
