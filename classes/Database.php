ea024a41e889b3dcf75a0b9ca4496d4cb26bf0fc
R3ndly <akalinart020@gmail.com>
2025-08-02 22:08:12 +0800

    Добавлен метод поиска коментариев

--- classes/Database.php
+++ classes/Database.php
@@ -13,4 +13,19 @@ class Database {
 	public function getConnection() {
 		return $this->pdo;
 	}
+
+	public function searchComments($searchText) {
+        $stmt = $this->pdo->prepare("
+            SELECT 
+                a.id AS post_id,
+                a.title AS post_title,
+                c.id AS comment_id, 
+                c.body AS comment_text
+            FROM comments c
+            JOIN articles a ON c.postId = a.id
+            WHERE c.body LIKE :search
+        ");
+        $stmt->execute(['search' => "%$searchText%"]);
+       	return $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    }
 }
